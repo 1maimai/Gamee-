347918bd941210ffa2f9dc0ec176b4b2e492b164
1maimai <163401313+1maimai@users.noreply.github.com>
2024-04-05 00:11:11 +0500

    Add files via upload

---
+++ PlayerCollision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            RestartGame();
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
